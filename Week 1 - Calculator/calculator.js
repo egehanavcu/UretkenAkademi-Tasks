243393221d14fc670083480f4e5c219723ff0a42
Egehan <egehanavcu@gmail.com>
2022-12-24 11:22:11 +0300

    Week 1 Task

---
+++ Week 1 - Calculator/calculator.js
@@ -0,0 +1,35 @@
+let result_text_div = document.querySelector(".result");
+let result_text = result_text_div.innerHTML;
+
+const updateText = function (text) {
+  result_text_div.innerHTML = result_text;
+};
+
+document.querySelector("#clear").addEventListener("click", function (event) {
+  result_text = "0";
+  updateText();
+});
+
+document.querySelector("#paranthesis").addEventListener("click", function () {
+  if (result_text[0] != "-" && result_text[0] != "0") {
+    result_text = "-" + result_text;
+  } else if (result_text[0] == "-") {
+    result_text = result_text.substring(1);
+  }
+  updateText();
+});
+
+document.querySelector("#equals").addEventListener("click", function (event) {
+  result_text_div.innerHTML = eval(result_text);
+  result_text = result_text_div.innerHTML;
+});
+
+document.querySelectorAll(".eval").forEach(function (number) {
+  number.addEventListener("click", function (event) {
+    if (result_text === "0") {
+      result_text = "";
+    }
+    result_text += event.target.id;
+    updateText();
+  });
+});
